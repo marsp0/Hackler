ce4bcad012893e7d16b9d3baa298bd4720d2e627
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-01-30 11:08:31 +0200

    v.24 - almost sorted is not passing all the cases

--- Implementation/almost_sorted.py
+++ Implementation/almost_sorted.py
@@ -1,23 +1,51 @@
 from collections import defaultdict
 
 n = int(raw_input().strip())
-array = [int(v) for v in raw_input().strip().split()]
-arrays = defaultdict(set)
-temp = 0
+#array = [int(v) for v in raw_input().strip().split()]
+array = [int(v) for v in open('test.txt','r').read().strip().split()]
 index = 0
+p1 = -1
+p2 = -1
+
 while index < n:
-	while True:
+	while index < n:
 		try:
-			if array[index] > array[index+1]:
-				arrays[temp].add(index)
-				arrays[temp].add(index+1)
+			if array[index] > array[index+1] and p1 == -1:
+				p1 = index
+				index += 1
+			elif array[index] > array[index+1] and p1 != -1:
 				index += 1
 			else:
-				break
+				if array[index] < array[index+1] and (p1 != -1 and array[index] < array[index-1]):
+					p2 = index
+					index += 1
+				else:
+					index += 1
+					break
 		except IndexError:
+			if p1 != -1 and p2 == -1:
+				p2 = index
+			index += 1
 			break
-	index  += 1
 
-if len(arrays.keys()) > 1:
-	print 'no'
-print arrays
+if p1 == -1 and p2 == -1:
+	if sorted(array) == array:
+		print 'yes'
+
+elif p2 - p1 == 1:
+	array[p2], array[p1] = array[p1], array[p2]
+	if sorted(array) == array:
+		print 'yes'
+		print 'swap {} {}'.format(p1+1, p2+1)
+	else:
+		print ';ds'
+		print 'no'
+elif p2 - p1 > 1:
+	temp = array[p1:p2+1]
+	temp = sorted(temp)
+	temp_array = array[:p1] + temp + array[p2+1:]
+	if temp_array == sorted(array):
+		print 'yes'
+		print 'reverse {} {}'.format(p1+1,p2+1)
+	else:
+		print 'no'
