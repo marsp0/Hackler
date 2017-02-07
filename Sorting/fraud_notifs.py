e0975a834330754d4fd10e5c3a8e64e5939411f5
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-02-07 21:35:30 +0200

    the fraud notifs is still not solved, but made huge progress thanks to a comment in the discussions of the challenge

--- Sorting/fraud_notifs.py
+++ Sorting/fraud_notifs.py
@@ -1,60 +1,33 @@
+import bisect
 from math import ceil
-import heapq as hq
-n, d = raw_input().strip().split()
-n,d = int(n), int(d)
-array = [int(v) for v in raw_input().strip().split()]
-#array = [int(v) for v in open('test.txt','r').read().strip().split()]
-array[:d] = sorted(array[:d])
+
+n , d = [int(v) for v in raw_input().strip().split()]
+#array = [int(v) for v in raw_input().strip().split()]
+array = [int(v) for v in open('test.txt','r').read().split()]
+d_array = []
 notifs = 0
-if d % 2 == 0:
-	max_heap = array[:d/2+1]
-	hq._heapify_max(max_heap)
 
-	min_heap = array[d/2+1:d]
-	hq.heapify(min_heap)
-else:
-	max_heap = array[:d/2+1]
-	hq._heapify_max(max_heap)
-	min_heap = array[d/2+1:d]
-	hq.heapify(min_heap)
+for index in xrange(d):
+	bisect.insort(d_array,array[index])
 
-for i in xrange(d,n):
-	#we need to check if notifs gets aumented
-	if d%2==1:
-		median = max_heap[0]
+for index in xrange(d,n):
+
+	if d % 2 == 0:
+
+		first = d_array[len(d_array)/2]
+		second = d_array[len(d_array)/2 - 1]
+		median = (first+second)/2
+
 	else:
-		median = (max_heap[0] + max_heap[1])/2
-	if 2*median <= array[i]:
+		median = d_array[len(d_array)/2]
+		#print len(d_array)/2 , median
+
+	if 2*median <= array[index]:
 		notifs += 1
 
-	if array[i-d] <= max_heap[0]:
-		print max_heap, min_heap, array[i]
-		to_remove = max_heap.index(array[i-d])
-		max_heap[-1], max_heap[to_remove] = max_heap[to_remove], max_heap[-1]
-		temp = max_heap[to_remove:-1]
-		hq._heapify_max(temp)
-		max_heap.pop(0)
-		max_heap[to_remove:] = temp
-	else:
-		to_remove = min_heap.index(array[i-d])
-		min_heap[-1], min_heap[to_remove] = min_heap[to_remove], min_heap[-1]
-		temp = min_heap[to_remove:-1]
-		hq.heapify(temp)
-		min_heap.pop(0)
-		min_heap[to_remove:] = temp
+	to_remove = bisect.bisect(d_array, array[index - d]) - 1
+	#print 2*median , array[index] , d_array
+	d_array.pop(to_remove)
+	bisect.insort(d_array, array[index])
 
-
-	if len(max_heap) != d/2 + 1:
-		if array[i] <= min_heap[0]:
-			hq.heappush(max_heap,array[i])
-		else:
-			temp = hq.heappop(min_heap)
-			hq.heappush(max_heap,temp)
-			hq.heappush(min_heap,array[i])
-	else:
-		if array[i] >= max_heap[0]:
-			hq.heappush(min_heap,array[i])
-		else:
-			temp = hq.heappop(max_heap)
-			hq.heappush(max_heap,temp)
-			hq.heappush(min_heap,array[i])
+print notifs
