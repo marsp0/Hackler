21c6a60cd408522c673d11a965f9df287f9d9b42
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-02-14 23:46:31 +0200

    v.33

--- Search/connected_cells.py
+++ Search/connected_cells.py
@@ -1,6 +1,6 @@
 ''' https://www.hackerrank.com/challenges/connected-cell-in-a-grid 
-	This challenge uses an algorithm called FloodFill. More info on it can be found here :
-	https://en.wikipedia.org/wiki/Flood_fill
+	This challenge uses an algorithm called flood_fill. More info on it can be found here :
+	https://en.wikipedia.org/wiki/flood_fill
 '''
 
 n = int(raw_input().strip())
@@ -9,4 +9,17 @@ graph = []
 for row in xrange(n):
 	graph.append([int(v) for v in raw_input().strip().split()])
 
-print graph
+def flood_fill(i,j):
+	if i >= n or j >= m or graph[i][j] == -1 or graph[i][j] == 0 or i< 0 or j < 0:
+		return 0
+	else:
+		graph[i][j] = -1
+		return 1 + flood_fill(i-1,j-1) + flood_fill(i-1,j) + flood_fill(i-1,j+1) + \
+				flood_fill(i,j-1) + flood_fill(i,j+1) + flood_fill(i+1,j-1) + flood_fill(i+1,j) + flood_fill(i+1,j+1)
+results = []
+for i in xrange(n):
+	for j in xrange(m):
+		temp = flood_fill(i,j)
+		results.append(temp)
+
+print max(results)
