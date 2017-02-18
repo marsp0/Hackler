c0c6ba9921efd3e8397f9eb6280543e7981f5292
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-02-18 12:39:26 +0200

    v.35

--- Search/count_lick.py
+++ Search/count_lick.py
@@ -1,7 +1,8 @@
 ''' https://www.hackerrank.com/challenges/count-luck '''
 
 ''' more info on backtracking here : https://en.wikipedia.org/wiki/Backtracking and https://www.cs.bu.edu/teaching/alg/maze/'''
-
+import subprocess
+import time
 tests = int(raw_input().strip())
 database = {}
 impressed = {}
@@ -24,40 +25,65 @@ for test in xrange(tests):
 	database[test] = temp
 
 def find_path(start_x,start_y,graph):
-	print start_y,start_x
-	for item in graph:
-		print item
+	#if you want to animate the process just uncomment these and watch the command line
+	#for item in graph:
+	#	print item
+	#subprocess.call(['clear'])
+	#time.sleep(1)
 	if start_x < 0 or start_x > len(graph[0])-1 or start_y < 0 or start_y > len(graph)-1:
-		print 'outside of graph'
 		return False
 	if graph[start_y][start_x] == '*':
-		print 'Goal'
 		return True
 	if graph[start_y][start_x] == 'X':
-		print 'Wall'
 		return False
-	if graph[start_y][start_x] in ('.','M'):
+	if graph[start_y][start_x] in ('.','M','*'):
 		graph[start_y][start_x] = '+'
-		for item in graph:
-			print item
-		print 'going north'
+		path.append((start_y,start_x))
 		if find_path(start_x,start_y-1,graph):
 			return True
-		print 'going east'
 		if find_path(start_x+1,start_y,graph):
 			return True
-		print 'going south'
 		if find_path(start_x,start_y+1,graph):
 			return True
-		print 'going west'
 		if find_path(start_x-1,start_y,graph):
 			return True
-	print 'Turning it down'
-	graph[start_y][start_x] = 'X'
+		graph[start_y][start_x] = '.'
+		path.pop()
 	return False
 
+def check_point(start_x,start_y):
+	if start_x < 0 or start_y < 0 or start_y > len(graph)-1 or start_x > len(graph[0])-1:
+		return 0
+	elif graph[start_y][start_x] in ('X','+','*'):
+		return 0
+	else:
+		return 1
+
+def check_path(path):
+	counter = 0
+	for item in path:
+		start_y,start_x = item
+		temp = 0
+		temp += check_point(start_x,start_y-1)
+		temp += check_point(start_x+1,start_y)
+		temp += check_point(start_x,start_y+1)
+		temp += check_point(start_x-1,start_y)
+		if temp > 0:
+			counter += 1
+	return counter
+
+
+results = []
 for test in database:
 	graph = database[test]
+	path = []
 	start_x,start_y = starts[test]
 	find_path(start_x,start_y,graph)
 	print graph
+	if impressed[test] == check_path(path):
+		results.append('Impressed')
+	else:
+		results.append('Oops!')
+
+for item in results:
+	print item
