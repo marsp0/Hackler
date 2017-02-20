a92eb01439a2bbe56e368f37562aec8622d5c47c
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-02-20 11:25:51 +0200

    v.37

--- Search/cut_tree.py
+++ Search/cut_tree.py
@@ -3,15 +3,22 @@
 	if we keep the min of the diff, after the completion the said min will be the correct one.
 '''
 import sys
-
-vertices = int(raw_input().strip())
-values  = [int(v) for v in raw_input().strip().split()]
+import gc
+sys.setrecursionlimit(100000)
+fish_filet = open('test.txt','r')
+#vertices = int(raw_input().strip())
+vertices = int(fish_filet.readline())
+#values  = [int(v) for v in raw_input().strip().split()]
+values = [int(v) for v in fish_filet.readline().strip().split()]
 edges = []
 graph = {}
 first_child = None
 second_child = None
+database = {}
+path = {}
 for i in xrange(vertices-1):
-	inputt = raw_input().strip().split()
+	#inputt = (v for v in raw_input().strip().split())
+	inputt = fish_filet.readline().strip().split()
 	edge = [int(v) for v in inputt]
 	edges.append(edge)
 
@@ -32,31 +39,38 @@ for edge in edges:
 		else:
 			second_child = first
 
+def get_sum(graph,start,parent):
+	global database
+	global path
+	summ = graph[start][0]
+	for child in graph[start][1]:
+		try:
+			path[child]
+		except KeyError:
+			if child != parent:
+				path[child] = 0
+				summ += get_sum(graph,child,start)
+	database[start] = summ
+	return summ
+
 def DFS(graph,start):
+	print '21321321'
 	min_sum = sys.maxsize
-	current_sum = 0
-	global values
+	path = {}
+	global database
 	total_sum = sum(values)
 	stack = [start]
-	visited = {}
-	path = []
 	while len(stack) > 0:
 		node = stack.pop()
-		try:
-			visited[node]
-		except KeyError:
-			visited[node] = 0
-			current_sum += graph[node][0]
-			total_sum -= graph[node][0]
-			if min_sum > abs(total_sum - current_sum):
-				min_sum = abs(total_sum - current_sum)
-			for edge in graph[node][1]:
+		if min_sum > abs((total_sum - database[node]) - database[node]):
+			min_sum = abs((total_sum - database[node]) - database[node])
+		for edge in graph[node][1]:
+			try:
+				path[edge]
+			except KeyError:
 				stack.append(edge)
-			temp = node
-			path.append(temp)
-	print path
+				path[edge] = 0
 	return min_sum
 
-
-
+get_sum(graph,1,None)
 print DFS(graph,1)
