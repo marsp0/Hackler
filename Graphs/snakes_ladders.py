0e7d3768396c26718b459c4c11012fc679833ac7
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-02-27 10:48:06 +0200

    v.44

--- Graphs/snakes_ladders.py
+++ Graphs/snakes_ladders.py
@@ -7,67 +7,97 @@ import sys
 from collections import deque
 
 def get_name(row,col):
-	return row*10+col
+	return row*10+col+1
 
 def get_permission(row,col):
 	if row < 0 or row > 9 or col < 0 or col > 9:
 		return False
 	return row,col
 
-graph = {}
+def get_graph():
+	graph = {}
 
-for i in xrange(10):
-	for j in xrange(10):
-		vertex = get_name(i,j)
-		graph[vertex] = [(i,j),[],None] 
+	for i in xrange(10):
+		for j in xrange(10):
+			vertex = get_name(i,j)
+			graph[vertex] = [(i,j),[],None] 
 
-for vertex in graph:
-	i,j = graph[vertex][0]
-	permissions = [ get_permission(i-1,j),
-					get_permission(i,j-1),
-					get_permission(i,j+1),
-					get_permission(i+1,j),
-				]
-	for item in xrange(len(permissions)):
-		if permissions[item] != False:
-			graph[vertex][1].append(get_name(permissions[item][0],permissions[item][1]))
+	for vertex in graph:
+		i,j = graph[vertex][0]
+		if j < 9:
+			graph[vertex][1].append(get_name(i,j+1))
+		else:
+			if i == 9 and j == 9:
+				pass
+			else:
+				graph[vertex][1].append(get_name(i+1,0))
+	return graph
 
 tests = int(raw_input().strip())
-database = [graph]*tests
-for test in database:
+database = []
+for i in xrange(tests):
+	temp = get_graph()
+	database.append(temp)
+snakes  = {}
+ladders = {}
+for index in xrange(tests):
+	snakes[index] = []
+	ladders[index] = []
 	n = int(raw_input().strip())
 	for i in xrange(n):
 		a,b = [int(v) for v in raw_input().strip().split()]
-		test[a][1].append(b)
+		ladders[index].append((a,b))
 	m = int(raw_input().strip())
 	for j in xrange(m):
 		a,b = [int(v) for v in raw_input().strip().split()]
-		test[a][1].append(b)
+		snakes[index].append((a,b))
+
+for index in xrange(len(database)):
+	for value in snakes[index]:
+		database[index][value[0]][1].append(value[1])
+	for value in ladders[index]:
+		database[index][value[0]][1].append(value[1])
 
 def get_result(graph):
 	current = 99
-	temp = 0
+	lista = []
 	while current != None:
-		temp += 1
-		print graph[current][2]
+		lista.append(current)
 		current = graph[current][2]
-	return temp
+	lista = list(reversed(lista))
+	print lista
+	result = 0
+	temp = 0
+	for index in xrange(len(lista)):
+		if index+1 < len(lista):
+			if lista[index+1] - lista[index] == 1:
+				temp += 1
+				if temp == 6:
+					result += 1
+					temp = 0
+			else:
+				result += 1
+				if temp != 0:
+					result += 1
+					temp = 0
+	return result
 
 for test in database:
-	path = {}
+	path = {1:0}
 	graph = test
-	queue = deque([(0,None)])
+	print graph
+	queue = deque([(1,None)])
 	while queue:
 		node, parent = queue.popleft()
+		#print node,parent
 		#print graph[node]
 		graph[node][2] = parent
 		#print graph[node]
 		if node == 99:
-			graph[0][2] = None
+			graph[1][2] = None
 			result = get_result(graph)
 		else:
 			for child in graph[node][1]:
-				if child not in path:
-					queue.append((child,node))
-					path[child] = 0
+				queue.append((child,node))
+				path[child] = 0
 	print result
