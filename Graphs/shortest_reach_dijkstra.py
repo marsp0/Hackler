7832a5fe5e5e6e1252bc69ac68eedb952c4f5a6d
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-03-03 11:25:01 +0200

    v.47

--- Graphs/shortest_reach_dijkstra.py
+++ Graphs/shortest_reach_dijkstra.py
@@ -1,21 +1,23 @@
+'''https://www.hackerrank.com/challenges/dijkstrashortreach'''
+
 import sys
-fish = open('test.txt', 'r')
-#tests = int(raw_input().strip())
-tests = int(fish.readline().strip())
-fillet = open('some.txt','w')
+
+#fish = open('test.txt', 'r')
+tests = int(raw_input().strip())
+#tests = int(fish.readline().strip())
 def key(dicta,item):
 	return dicta[item]
 
 for test in xrange(tests):
-	#n,m = [int(v) for v in raw_input().strip().split()]
-	n,m = [int(v) for v in fish.readline().strip().split()]
+	n,m = [int(v) for v in raw_input().strip().split()]
+	#n,m = [int(v) for v in fish.readline().strip().split()]
 	graph = {key:{} for key in xrange(1,n+1)}
 	distances = {key:sys.maxsize for key in xrange(1,n+1)}
 	predecessors = {key:key for key in xrange(1,n+1)}
 	temp = None
 	for i in xrange(m):
-		#a,b,w = [int(v) for v in raw_input().strip().split()]
-		a,b,w = [int(v) for v in fish.readline().strip().split()]
+		a,b,w = [int(v) for v in raw_input().strip().split()]
+		#a,b,w = [int(v) for v in fish.readline().strip().split()]
 		if b in graph[a]:
 			if graph[a][b] > w:
 				graph[a][b] = w
@@ -23,9 +25,8 @@ for test in xrange(tests):
 		else:
 			graph[a][b] = w
 			graph[b][a] = w
-#	start_node = int(raw_input().strip())
-	start_node = int(fish.readline().strip())
-	fillet.write('start node is {}\n'.format(start_node))
+	start_node = int(raw_input().strip())
+	#start_node = int(fish.readline().strip())
 	distances[start_node] = 0
 	unvisited_set = set(graph.keys())
 	current = start_node
@@ -34,25 +35,20 @@ for test in xrange(tests):
 		for edge in graph[current]:
 			if distances[current] + graph[current][edge] < distances[edge]:
 				distances[edge] = distances[current] + graph[current][edge]
-				fillet.write('updating predecessor {} for {} \n'.format(current, edge))
 				predecessors[edge] = current
-		current = None
-		fillet.write('searching for the next min\n')
-		fillet.write('{}\n'.format(distances))
-		for element in distances:
-			fillet.write('checking {},{} and {} and {} is in unvisited {}\n'.format(element,distances[element],current,element, element in unvisited_set))
-			if current == None:
-				if element in unvisited_set:
-					current = element
-			elif distances[element] < distances[current] and element in unvisited_set:
-				current = element
-		fillet.write('the next min is {}\n'.format(current))
-		if current == sys.maxsize:
+		current = min(distances.items(), key = lambda x: x[1] if x[0] in unvisited_set else sys.maxsize)
+		if current[1] == sys.maxsize:
 			break
-		unvisited_set.remove(current)
+		else:
+			current = current[0]
+		try:
+			unvisited_set.remove(current)
+		except KeyError:
+			break
 	for item in distances:
 		if item != start_node:
 			if distances[item] == sys.maxsize:
 				print -1,
 			else:
 				print distances[item],
+		print
