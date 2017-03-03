2f0548de651fea7e05943c7b071e2a80dcd18cd0
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-03-03 10:14:28 +0200

    v.46

--- Graphs/shortest_reach_dijkstra.py
+++ Graphs/shortest_reach_dijkstra.py
@@ -2,6 +2,10 @@ import sys
 fish = open('test.txt', 'r')
 #tests = int(raw_input().strip())
 tests = int(fish.readline().strip())
+fillet = open('some.txt','w')
+def key(dicta,item):
+	return dicta[item]
+
 for test in xrange(tests):
 	#n,m = [int(v) for v in raw_input().strip().split()]
 	n,m = [int(v) for v in fish.readline().strip().split()]
@@ -21,6 +25,7 @@ for test in xrange(tests):
 			graph[b][a] = w
 #	start_node = int(raw_input().strip())
 	start_node = int(fish.readline().strip())
+	fillet.write('start node is {}\n'.format(start_node))
 	distances[start_node] = 0
 	unvisited_set = set(graph.keys())
 	current = start_node
@@ -29,11 +34,19 @@ for test in xrange(tests):
 		for edge in graph[current]:
 			if distances[current] + graph[current][edge] < distances[edge]:
 				distances[edge] = distances[current] + graph[current][edge]
+				fillet.write('updating predecessor {} for {} \n'.format(current, edge))
 				predecessors[edge] = current
-		current = sys.maxsize
+		current = None
+		fillet.write('searching for the next min\n')
+		fillet.write('{}\n'.format(distances))
 		for element in distances:
-			if distances[element] < current and element in unvisited_set:
+			fillet.write('checking {},{} and {} and {} is in unvisited {}\n'.format(element,distances[element],current,element, element in unvisited_set))
+			if current == None:
+				if element in unvisited_set:
+					current = element
+			elif distances[element] < distances[current] and element in unvisited_set:
 				current = element
+		fillet.write('the next min is {}\n'.format(current))
 		if current == sys.maxsize:
 			break
 		unvisited_set.remove(current)
@@ -42,7 +55,4 @@ for test in xrange(tests):
 			if distances[item] == sys.maxsize:
 				print -1,
 			else:
-				print distances[item], 
-	print
-	#if test == tests-1:
-	#	current = 
+				print distances[item],
