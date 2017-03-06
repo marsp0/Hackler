fea6c1a1cc314f7724433d147f800b9f97c12670
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-03-06 14:50:30 +0200

    v.52

--- Graphs/jack_rapture.py
+++ Graphs/jack_rapture.py
@@ -2,50 +2,47 @@ import sys
 
 n,m = [int(v) for v in raw_input().strip().split()]
 graph = {key:{} for key in xrange(1,n+1)}
+edges = []
 for i in xrange(m):
 	a,b,w = [int(v) for v in raw_input().strip().split()]
 	graph[a][b] = w
-	#graph[b][a] = w
+	graph[b][a] = w
+	edges.append((a,b,w))
 
-distances = {key:sys.maxsize for key in xrange(1,n+1)}
-distances[1] = 0
-predecessors = {key:None for key in xrange(1,n+1)}
-seen = {key:False for key in xrange(1,n+1)}
-def get_sum(predecessors, element):
-	temp = 0
-	while element != None:
-		#print 'element is ', element, distances[element]
-		#print predecessors
-		temp += distances[element]
-		element = predecessors[element]
-	return temp
+inside = [[key] for key in xrange(1,n+1)]
+edges = sorted(edges, key = lambda x: x[2])
+result = []
+for edge in edges:
+	first, second, weight = edge
+	f_index = -1
+	s_index = -1
+	for element in xrange(len(inside)):
+		if f_index == -1:
+			if first in inside[element]:
+				f_index = element
+		if s_index == -1:
+			if second in inside[element]:
+				s_index = element
+	if f_index != s_index:
+		result.append(edge)
+		inside[f_index].extend(inside[s_index])
+new_graph = {key:{} for key in xrange(1,n+1)}
+for item in result:
+	a,b,w = item
+	new_graph[a][b] = w
+	new_graph[b][a] = w
 
-vertices = {key:True for key in xrange(1,n+1)}
-
-while vertices:
-	node = min(distances.items(), key = lambda x: x[1] if x[0] in vertices else sys.maxsize)[0]
-	del vertices[node]
-	seen[node] = True
-	#print distances
-	if node != sys.maxsize:
-		for edge in graph[node]:
-			if not seen[edge]:
-				prev_distances = get_sum(predecessors,node)
-				#print node, edge, graph[node][edge], prev_distances
-				if graph[node][edge] - prev_distances < distances[edge]:
-					if graph[node][edge] - prev_distances < 0:
-						distances[edge] = 0
-					else:
-						distances[edge] = graph[node][edge] - prev_distances
-					#print node, edge
-					predecessors[edge] = node
-	else:
+stack = [1]
+visited = {}
+predecessor = {1:None}
+while stack:
+	node = stack.pop()
+	visited = {1:True}
+	if node == n:
 		break
-current = n
-result = 0
-while current != None:
-	print current
-	result += distances[current]
-	current = predecessors[current]
+	for edge in new_graph[node]:
+		if edge not in visited:
+			stack.append(edge)
+			predecessor[edge] = node
 
-print result
+print new_graph
