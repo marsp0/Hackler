9a5ee06fa657cde8ce0bda590cf8a8a7f70d9778
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-03-06 12:23:39 +0200

    v.51

--- Graphs/jack_rapture.py
+++ Graphs/jack_rapture.py
@@ -5,7 +5,7 @@ graph = {key:{} for key in xrange(1,n+1)}
 for i in xrange(m):
 	a,b,w = [int(v) for v in raw_input().strip().split()]
 	graph[a][b] = w
-	graph[b][a] = w
+	#graph[b][a] = w
 
 distances = {key:sys.maxsize for key in xrange(1,n+1)}
 distances[1] = 0
@@ -26,19 +26,26 @@ while vertices:
 	node = min(distances.items(), key = lambda x: x[1] if x[0] in vertices else sys.maxsize)[0]
 	del vertices[node]
 	seen[node] = True
-	print distances
+	#print distances
 	if node != sys.maxsize:
 		for edge in graph[node]:
 			if not seen[edge]:
 				prev_distances = get_sum(predecessors,node)
-				print node, edge, graph[node][edge], prev_distances
+				#print node, edge, graph[node][edge], prev_distances
 				if graph[node][edge] - prev_distances < distances[edge]:
 					if graph[node][edge] - prev_distances < 0:
 						distances[edge] = 0
 					else:
 						distances[edge] = graph[node][edge] - prev_distances
-					print node, edge
+					#print node, edge
 					predecessors[edge] = node
 	else:
 		break
-print distances
+current = n
+result = 0
+while current != None:
+	print current
+	result += distances[current]
+	current = predecessors[current]
+
+print result
