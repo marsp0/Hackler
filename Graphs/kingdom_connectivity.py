8a4489018dac0d60a164da093d572ce9caf4507f
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-03-12 18:06:16 +0200

    v.62

--- Graphs/kingdom_connectivity.py
+++ Graphs/kingdom_connectivity.py
@@ -1,22 +1,22 @@
+import math
+
 n,m = [int(v) for v in raw_input().strip().split()]
 graph = {key:[] for key in xrange(1,n+1)}
 for i in xrange(m):
 	a,b = [int(v) for v in raw_input().strip().split()]
 	graph[b].append(a)
 
-memo = {1:0}
+memo = {1:1}
 
 def get_value(memo,graph,node):
-	print 'checking if {} is in memo : {}'.format(node,node in memo)
 	if node in memo:
 		return memo[node]
 	else:
-		result = len(graph[node])
-		print 'iterating over the edges'
+		result = 0
 		for edge in graph[node]:
 			result += get_value(memo,graph,edge)
 		memo[node] = result
 		return result
 
 get_value(memo,graph,n)
-print memo
+print int(memo[n]%(math.pow(10,9)))
