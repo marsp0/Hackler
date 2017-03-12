64e05fca1b693c95299facb8316e3fececa9a502
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-03-12 15:54:16 +0200

    v.61

--- Graphs/kingdom_connectivity.py
+++ Graphs/kingdom_connectivity.py
@@ -2,16 +2,21 @@ n,m = [int(v) for v in raw_input().strip().split()]
 graph = {key:[] for key in xrange(1,n+1)}
 for i in xrange(m):
 	a,b = [int(v) for v in raw_input().strip().split()]
-	graph[a].append(b)
-paths = {}
-def dfs(u,t):
-	if u == t:
-		paths[u] = 1
-	else:
-		if u not in paths:
-			paths[u] = sum(dfs(c,t) for c in graph[u])
-	return paths[u]
+	graph[b].append(a)
 
-dfs(1,n)
-print graph
-print paths
+memo = {1:0}
+
+def get_value(memo,graph,node):
+	print 'checking if {} is in memo : {}'.format(node,node in memo)
+	if node in memo:
+		return memo[node]
+	else:
+		result = len(graph[node])
+		print 'iterating over the edges'
+		for edge in graph[node]:
+			result += get_value(memo,graph,edge)
+		memo[node] = result
+		return result
+
+get_value(memo,graph,n)
+print memo
