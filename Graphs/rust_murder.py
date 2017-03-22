9f590cd577a921e24f355be55e3d3ba836252069
Martin Spasov <Suburb4nFilth@gmail.com>
2017-03-22 21:13:22 +0200

    v.74

--- Graphs/rust_murder.py
+++ Graphs/rust_murder.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 tests = int(raw_input().strip())
 test_database = []
 for i in xrange(tests):
@@ -18,4 +20,28 @@ for i in xrange(tests):
 
 for test in test_database:
 	graph,start_node = test
-	
+	distances = {key:0 for key in graph.keys()}
+	visited = {start_node:True}
+	# 1 would be white
+	# 2 would be grey
+	# 3 would be black
+	colors = {key:1 for key in graph.keys()}
+	before_list = {key:0 for key in graph.keys()}
+	after_list = {}
+	queue = deque([start_node])
+	while queue:
+		u = queue.popleft()
+		for edge in graph[u]:
+			if colors[edge] == 1:
+				del before_list[edge]
+				after_list[edge] = 0
+		for key in before_list:
+			colors[key] = 2
+			if not key in visited:
+				distances[key] = distances[u] + 1
+				queue.append(key)
+		colors[u] = 3
+		before_list = after_list
+		after_list = {}
+
+	print distances
