5a73da58021ad15716e38dc1bba0471c0b62db49
Martin Spasov <Suburb4nFilth@gmail.com>
2017-03-25 00:06:25 +0200

    v.76

--- Graphs/clique.py
+++ Graphs/clique.py
@@ -4,15 +4,20 @@ import sys
 tests = int(raw_input().strip())
 #fish = open('test.txt','r')
 #tests = int(fish.readline())
+temp = []
 for i in xrange(tests):
 	n,m = [float(v) for v in raw_input().strip().split()]
 	#n,m = [float(int(v)) for v in fish.readline().split()]
 	result = sys.maxsize
 	r = n - 1
+	squared_n = n*n
 	while result > m:
-		result = (n*n - (n%r)*int(ceil(n/r)**2) - (r - (n%r))*int(n/r)**2)/2
-		r -= 1
+		result = (squared_n - (n%r)*int(ceil(n/float(r))**2) - (r - (n%r))*int(n/r)**2)/2					
+		if result >= m:
+			r = r - 1
 		r = int(r)
 		n = int(n)
-		print result, 'result'
-	print r + 1
+	temp.append(r+1)
+
+for item in temp:
+	print item
