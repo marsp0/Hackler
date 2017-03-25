d217d136df93c6e4e6596ebeaf78bf386043191a
Martin Spasov <Suburb4nFilth@gmail.com>
2017-03-25 11:43:34 +0200

    v.77

--- Graphs/clique.py
+++ Graphs/clique.py
@@ -8,16 +8,22 @@ temp = []
 for i in xrange(tests):
 	n,m = [float(v) for v in raw_input().strip().split()]
 	#n,m = [float(int(v)) for v in fish.readline().split()]
-	result = sys.maxsize
-	r = n - 1
+	r = floor(n*float(n) / (n*n - 2*m))
 	squared_n = n*n
-	while result > m:
-		result = (squared_n - (n%r)*int(ceil(n/float(r))**2) - (r - (n%r))*int(n/r)**2)/2					
-		if result >= m:
-			r = r - 1
+	while True:
+		result = (squared_n - (n%r)*int(ceil(n/float(r))**2) - (r - (n%r))*int(n/r)**2)/2
+		r = r + 1
+		if result >= m or r > n:
+			break
 		r = int(r)
 		n = int(n)
-	temp.append(r+1)
+	temp.append(r-1)
 
 for item in temp:
-	print item
+	print int(item)
+
+
+#m * 2/n^2 = (r-1)/r
+#m * 2/n^2 = 1 - 1/r
+#1 - 2m/ n^2= 1/r
+#r = n^2/n^2 - 2m
