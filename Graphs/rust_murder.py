398b2945ed4a56d01c6877a4beb57833acbb20c9
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-03-31 23:06:38 +0300

    v.84

--- Graphs/rust_murder.py
+++ Graphs/rust_murder.py
@@ -1,21 +1,22 @@
-tests = int(raw_input().strip())
+import sys
+from collections import deque
+fish = open('test.txt','r')
+#tests = int(raw_input().strip())
+tests = int(fish.readline().strip())
 test_database = []
 for i in xrange(tests):
-	n,m = [int(v) for v in raw_input().strip().split()]
-	temp = {key:{} for key in xrange(1,n+1)}
-	for j in xrange(m):
-		a,b = [int(v) for v in raw_input().strip().split()]
-		temp[a][b] = 0
-		temp[b][a] = 0
-	start_node = int(raw_input().strip())
+	#n,m = [int(v) for v in raw_input().strip().split()]
+	n,m = [int(v) for v in fish.readline().strip().split()]
 	graph = {key:{} for key in xrange(1,n+1)}
-	for key in temp:
-		for i in xrange(1,n+1):
-			if not i in temp[key] and not i in graph[key] and i != key:
-				graph[key][i] = 0
-				graph[i][key] = 0
+	for j in xrange(m):
+		#a,b = [int(v) for v in raw_input().strip().split()]
+		a,b = [int(v) for v in fish.readline().strip().split()]
+		if a == 33498 or b == 33498:
+			print a,b
+		graph[a][b] = 0
+		graph[b][a] = 0
+	#start_node = int(raw_input().strip())
+	start_node = int(fish.readline().strip())
+	print start_node
 	test_database.append((graph,start_node))
-
-for test in test_database:
-	graph,start_node = test
-	
+print len(test_database)
