0fd1515bfcfd118d41917c4e2b7086e1135ec972
suburbanfilth <suburb4nfilth@gmail.com>
2017-04-01 17:19:27 +0300

    v.86

--- Implementation/absolute_permutations.py
+++ Implementation/absolute_permutations.py
@@ -23,4 +23,41 @@ for test in test_database:
 				positions[number - k].append(number)
 		except KeyError:
 			pass
-	print positions
+	results = [0] * n
+	doubles = []
+	placed = {}
+	for i in xrange(1,n+1):
+		value_list = positions[i]
+		if not value_list:
+			print -1
+			break
+		else:
+			if len(value_list) == 1:
+				results[i-1] = value_list[0]
+				placed[value_list[0]] = True
+			else:
+				doubles.append(i)
+	index = 0
+	while doubles:
+		print index, doubles, results
+		try:
+			position_number = doubles[index]
+			print position_number
+			possible_numbers = positions[doubles[index]]
+			print possible_numbers
+			for item in xrange(len(possible_numbers)):
+				print possible_numbers[item], placed, 'dsads'
+				if possible_numbers[item] in placed:
+					print 'was here'
+					if item == 0:
+						results[position_number] = possible_numbers[item+1]
+						placed[possible_numbers[item+1]] = True
+						doubles.pop(index)
+					else:
+						results[position_number] = possible_numbers[item-1]
+						placed[possible_numbers[item-1]] = True
+						doubles.pop(index)
+			index += 1
+		except IndexError:
+			index = 0
+	print results
