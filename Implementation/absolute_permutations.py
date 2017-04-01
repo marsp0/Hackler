fa90462f02ba71981aad7bc86f094336e2d8df61
suburbanfilth <suburb4nfilth@gmail.com>
2017-04-01 22:43:35 +0300

    v.88

--- Implementation/absolute_permutations.py
+++ Implementation/absolute_permutations.py
@@ -11,51 +11,19 @@ for test in test_database:
 			print i,
 		print
 		continue
-	positions = {key:[] for key in xrange(1,n+1)}
-	for number in xrange(1,n+1):
-		try:
-			if number + k < n+1 and n >= 1:
-				positions[number + k].append(number)
-		except KeyError:
-			pass
-		try:
-			if number + k < n+1 and n >= 1:
-				positions[number - k].append(number)
-		except KeyError:
-			pass
-	results = [0] * n
-	doubles = []
-	placed = {}
-	for i in xrange(1,n+1):
-		value_list = positions[i]
-		if not value_list:
-			print -1
-			break
-		else:
-			if len(value_list) == 1:
-				results[i-1] = value_list[0]
-				placed[value_list[0]] = True
-			else:
-				doubles.append(i)
-	index = 0
-	while doubles:
-		try:
-			position_number = doubles[index]
-			possible_numbers = positions[position_number]
-			for item in xrange(len(possible_numbers)):
-				if possible_numbers[item] in placed:
-					if item == 0:
-						results[position_number-1] = possible_numbers[item+1]
-						placed[possible_numbers[item+1]] = True
-					else:
-						results[position_number-1] = possible_numbers[item-1]
-						placed[possible_numbers[item-1]] = True
-					doubles.pop(index)
-					break
-			index += 1
-		except IndexError:
-			index = 0
-	for item in results:
-		print item,
-	print n,k
+	elif n % k != 0:
+		print -1
+		continue
+	elif n % 2 == 1:
+		print -1
+		continue
+
+	results = [0] * (n+1)
+	for i in xrange(1,len(results)):
+		if results[i] == 0:
+			results[i] = i+k
+			results[i+k] = i
+	for i in xrange(len(results)):
+		if i != 0:
+			print results[i],
 	print
