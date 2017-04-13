025f8ae5f4d43978932363733ab01002df90a21b
suburbanfilth <suburb4nfilth@gmail.com>
2017-04-14 00:11:09 +0300

    v.96

--- Dynamic Programming/substring_sam.py
+++ Dynamic Programming/substring_sam.py
@@ -1,5 +1,8 @@
+import math
+
 #Input from the exercise
 input_str = raw_input().strip()
+#input_str = open('test.txt','r').read()
 
 #we separate string into and make it a list
 separated_list = []
@@ -10,29 +13,29 @@ memo = {}
 temp_sum = 0
 for i in xrange(len(separated_list)):
 	temp_sum += int(separated_list[i])
-	memo[(i+1,0)] = temp_sum
 	memo[(0,i+1)] = temp_sum
 	memo[(i,i)] = 0
 
 memo[(0,0)] = 0
 memo[(0,len(separated_list))] = temp_sum 
-memo[(len(separated_list),0)] = temp_sum 
 memo[(len(separated_list), len(separated_list))] = 0
 
-lower_result = temp_sum
-higher_result = temp_sum
+mod_value = int(math.pow(10,9)+7)
 flag = False
-for i in xrange(len(separated_list)+1):
-	for j in xrange(len(separated_list)+1):
-		if i == j or (i,j) in memo:
-			if i ==j:
-				flag = True
-			continue
-		if j > i:
-			if flag == True:
-				memo[(i,j)] = memo[(i-1,len(separated_list))] + int(''.join(separated_list[i-1:j]))
-				flag = False
-			else:
-				memo[(i,j)] = memo[(i,j-1)] + int(''.join(separated_list[i-1:j]))
+result = temp_sum
+#for i in xrange(len(separated_list)+1):
+#	for j in xrange(len(separated_list)+1):
+#		if i == j or (i,j) in memo:
+#			continue
+#		if j > i:
+#			result = (result + int(input_str[i-1:j])) % mod_value
+#			print result
 
-print memo[len(separated_list)-1, len(separated_list)]
+result = {-1:0}
+for i in xrange(len(separated_list)):
+	print i
+	temp = 10*result[i-1] + (i+1)*int(separated_list[i]) % mod_value
+	result[i] = temp
+
+
+print sum(result.values()) % mod_value
