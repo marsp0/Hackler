04dd14a5c770f06e753ff5c9a8f1d5b06a3a6e11
suburbanfilth <suburb4nfilth@gmail.com>
2017-04-30 23:54:52 +0300

    v.104

--- Dynamic Programming/longest_increasing_subsequence.py
+++ Dynamic Programming/longest_increasing_subsequence.py
@@ -1,12 +1,26 @@
+'''https://www.hackerrank.com/challenges/longest-increasing-subsequent - MEDIUM'''
+
 n= int(raw_input().strip())
 array = []
 for i in xrange(n):
 	array.append(int(raw_input().strip()))
 
-memo = {}
+def longest_increasing_subsequence(array):
+    tails = [0] * len(array)
+    size = 0
+    for x in array:
+    	print 'X is ',x
+    	print 'tails is', tails
+        i, j = 0, size
+        print 'i,j is ',i,j
+        while i != j:
+            m = (i + j) / 2
+            if tails[m] < x:
+                i = m + 1
+            else:
+                j = m
+        tails[i] = x
+        size = max(i + 1, size)
+    return size
 
-def longest_increasing_subsequence(array,memo):
-
-
-
-print max_subarray_contiguous(array)
+print longest_increasing_subsequence(array)
