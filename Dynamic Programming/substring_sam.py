4aa3b6466f24d5fba46837382a615d8bdadb373e
suburbanfilth <suburb4nfilth@gmail.com>
2017-06-01 19:00:17 +0300

    v.121

--- Dynamic Programming/substring_sam.py
+++ Dynamic Programming/substring_sam.py
@@ -1,40 +1,16 @@
-import math
+'''https://www.hackerrank.com/challenges/sam-and-substrings - medium'''
 
-#Input from the exercise
-#input_str = raw_input().strip()
-input_str = open('test.txt','r').read()
+from collections import defaultdict
 
-#we separate string into and make it a list
-separated_list = []
-for element in input_str:
-	separated_list.append(element)
+number = raw_input().strip()
+prev_s_i = 0
+prev_t_i = 0
+mod = 10**9 + 7
+for i in xrange(len(number)):
+	s_i = (10*prev_s_i + (i+1)*int(number[i]) )%mod
+	t_i = (prev_t_i + s_i) % mod
+	#print s_i, t_i
+	prev_t_i = t_i
+	prev_s_i = s_i
 
-memo = {}
-temp_sum = 0
-for i in xrange(len(separated_list)):
-	temp_sum += int(separated_list[i])
-	memo[(0,i+1)] = temp_sum
-	memo[(i,i)] = 0
-
-memo[(0,0)] = 0
-memo[(0,len(separated_list))] = temp_sum 
-memo[(len(separated_list), len(separated_list))] = 0
-
-mod_value = int(math.pow(10,9)+7)
-flag = False
-result = temp_sum
-#for i in xrange(len(separated_list)+1):
-#	for j in xrange(len(separated_list)+1):
-#		if i == j or (i,j) in memo:
-#			continue
-#		if j > i:
-#			result = (result + int(input_str[i-1:j])) % mod_value
-#			print result
-
-result = {-1:0}
-for i in xrange(len(separated_list)):
-	temp = 10*result[i-1] + (i+1)*int(separated_list[i]) % mod_value
-	result[i] = temp
-
-
-print sum(result.values()) % mod_value
+print t_i % mod
